c711f68c564fe24a77758ca1129a5218eed29612
Mark <schuller.mark@thisisdone.com>
2023-03-07 10:14:39 +0100

    remove highlight js

--- src/routes/components/quiz/Question.svelte
+++ src/routes/components/quiz/Question.svelte
@@ -1,13 +1,12 @@
 <script>
     export let questionText;
     export let code;
-    import hljs from "highlight.js";
 </script>
 
 <h1 class="text-5xl mb-8 font-bold">{questionText}</h1>
 
 {#if code}
     <div class="mb-5 p-4 bg-info-content">
-        {@html hljs.highlightAuto(code).value}
+        {code}
     </div>
 {/if}
